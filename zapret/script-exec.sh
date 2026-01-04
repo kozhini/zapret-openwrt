1692ca548c04cfb65d1088cc10b57603c143d5b4
kozhini <131589545+kozhini@users.noreply.github.com>
2026-01-05 00:25:04 +0700

    Update script-exec.sh

--- zapret/script-exec.sh
+++ zapret/script-exec.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Copyright (c) 2024 remittor
 LOG_FILE=$1
-PID_FILE=$2
-shift 2
+RC_FILE=$1.rc
+shift 1
 : > $LOG_FILE
+: > $RC_FILE
 (
 	exec </dev/null >/dev/null 2>&1
 	"$@" >> $LOG_FILE 2>&1
+	RETCODE=$?
+	sleep 1
+	echo $RETCODE > $RC_FILE
 ) &
-echo $! > $PID_FILE
 exit 0
